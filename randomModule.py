8b06f3ec4f41220e4f4959230ed3665b753e5203
Zach Ruiz <powersbooth10@gmail.com>
2023-06-19 12:09:11 -0500

    practice code added.

---
+++ randomModule.py
@@ -0,0 +1,10 @@
+import random
+
+random_integer = random.randint(1, 10)
+# print(random_integer)
+
+random_float = random.random()
+# print(random_float)
+
+random_uniform = random.uniform(1, 5)
+print(random_uniform)
